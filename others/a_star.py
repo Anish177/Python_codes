e087f96f3d0256488b965214e08f2625006c8ce6
Anish177 <30626499+Anish177@users.noreply.github.com>
2023-08-30 16:23:22 +0530

    Update a_star.py
    Now with diagonal movement

--- others/a_star.py
+++ others/a_star.py
@@ -79,17 +79,27 @@ class Node:
 
     def update_neighbors(self, grid):
         self.neighbors = []
-        if self.row < self.total_rows - 1 and not grid[self.row + 1][self.col].is_barrier():  # Down
+
+        # # Orthogonal neighbors
+        if self.row < self.total_rows - 1 and not grid[self.row + 1][self.col].is_barrier():
             self.neighbors.append(grid[self.row + 1][self.col])
-
-        if self.row > 0 and not grid[self.row - 1][self.col].is_barrier():  # Up
+        if self.row > 0 and not grid[self.row - 1][self.col].is_barrier():
             self.neighbors.append(grid[self.row - 1][self.col])
-
-        if self.col < self.total_rows - 1 and not grid[self.row][self.col + 1].is_barrier():  # Right
+        if self.col < self.total_rows - 1 and not grid[self.row][self.col + 1].is_barrier():
             self.neighbors.append(grid[self.row][self.col + 1])
-
-        if self.col > 0 and not grid[self.row][self.col - 1].is_barrier():  # Left
+        if self.col > 0 and not grid[self.row][self.col - 1].is_barrier():
             self.neighbors.append(grid[self.row][self.col - 1])
+
+        # Diagonal neighbors
+        if self.row < self.total_rows - 1 and self.col < self.total_rows - 1 and \
+                not grid[self.row + 1][self.col + 1].is_barrier():
+            self.neighbors.append(grid[self.row + 1][self.col + 1])
+        if self.row > 0 and self.col > 0 and not grid[self.row - 1][self.col - 1].is_barrier():
+            self.neighbors.append(grid[self.row - 1][self.col - 1])
+        if self.row < self.total_rows - 1 and self.col > 0 and not grid[self.row + 1][self.col - 1].is_barrier():
+            self.neighbors.append(grid[self.row + 1][self.col - 1])
+        if self.row > 0 and self.col < self.total_rows - 1 and not grid[self.row - 1][self.col + 1].is_barrier():
+            self.neighbors.append(grid[self.row - 1][self.col + 1])
 
     def __lt__(self, other):
         return False
@@ -98,7 +108,7 @@ class Node:
 def h(p1, p2):
     x1, y1 = p1
     x2, y2 = p2
-    return abs(x1 - x2) + abs(y1 - y2)
+    return ((abs(x1 - x2))**2 + (abs(y1 - y2))**2)**0.5
 
 # Reconstruct the path
 def reconstruct_path(came_from, current, draw):
